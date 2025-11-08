52c05365076192e80c7b7a7bfaada0cf92c34571
Tiago Shikota <th.shikota@gmail.com>
2025-11-08 16:38:35 -0300

    Criando a informação da despesa

--- scripts.js
+++ scripts.js
@@ -1,42 +1,41 @@
-// Seleciona os elementos do formulário.
+// Seleciona os elementos do formulário
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
-// Seleciona os elementos da lista.
+// Seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
 
-// Captura o evento de input para formatar o valor.
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
-    // Obtém o valor atual do input e remove os caracteres não numéricos;.
+    // Obtém o valor atual do input e remove os caracteres não numéricos
     let value = amount.value.replace(/\D/g, "")
 
-    // Transformar o valor em centavos.
+    // Transformar o valor em centatos (exemplo: 150/100 = 1.5 é equivalente a R$ 1,50)
     value = Number(value) / 100
-    
+
     // Atualiza o valor do input
     amount.value = formatCurrencyBRL(value)
 }
 
-function formatCurrencyBRL(value){
+function formatCurrencyBRL(value) {
     // Formata o valor no padrão BRL (Real Brasileiro)
     value = value.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL"
     })
 
-    // Retorna o valor formatado.
+    // Retorna o valor formatado 
     return value
 }
 
-
-// Captura o evento de submit do formulário para obter os valores.
+// Captura o evento de submit do formulário para obter os valores 
 form.onsubmit = (event) => {
-    // Previne o comportamento padrão de recarregar a página.
+    // Previne o comportamento padrão de recarregar a página
     event.preventDefault()
 
-    // Cria um objeto com os detalhes da nova despesa.
+    // Cria um objeto com os detalhes da nova despesa
     const newExpense = {
         id: new Date().getTime(),
         expense: expense.value,
@@ -46,30 +45,44 @@ form.onsubmit = (event) => {
         created_at: new Date(),
     }
 
-    // Chama a função que irá adicionar o item na lista.
-    expenseADD(newExpense)
+    // Chama a função que irá adicionar o item na lista
+    expenseAdd(newExpense)
 }
 
-function expenseADD(newExpense) {
+function expenseAdd(newExpense) {
     try {
-        // Cria o elemento de <li> para adicionar o item na lista(<ul>).
-        const expenseItem = document. createElement("li")
+        // Cria o elemento para a adicionar o item (li) na lista (ul)
+        const expenseItem = document.createElement("li")
         expenseItem.classList.add("expense")
 
-        // Cria o ícone da categoria.
+        // Cria o ícone da categoria
         const expenseIcon = document.createElement("img")
         expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
         expenseIcon.setAttribute("alt", newExpense.category_name)
 
-        // Adiciona as informações do item.
-        expenseItem.append(expenseIcon)
+        // Cria a info da despesa
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
 
-        // Adiciona o item na lista.
-        expenseItem.append(expenseItem)
+        // Cria o nome da despesa
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        // Cria a categoria da despesa
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        // Adiciona o nome e a categoria na div das informações da despesa
+        expenseInfo.append(expenseName, expenseCategory)
+
+        // Adiciona as informações no item
+        expenseItem.append(expenseIcon, expenseInfo)
+
+        // Adiciona o item na lista
+        expenseList.append(expenseItem)
 
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
         console.log(error)
     }
 }
-
